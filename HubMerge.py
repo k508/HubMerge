7ae54f1c2734a7aaf03ba1e64565e6278cb397c3
k508 <44594320+k508@users.noreply.github.com>
2020-03-03 15:00:26 +1100

    Updated readme

--- HubMerge.py
+++ HubMerge.py
@@ -13,22 +13,10 @@ b.rename(columns={'Associated Contact IDs': 'Contact ID'}, inplace=True)
 merged = a.merge(b, on="Contact ID")
 merged.to_csv("output.csv", index=False)
 
-filtered_csv = pd.read_csv("output.csv", usecols=['Phone Number',
-                                                  'Appointment State/Region',
-                                                  'Appointment Date',
-                                                  'Appointment Street Address',
-                                                  'Consultant',
-                                                  'Appointment City',
-                                                  'Conference Venues',
-                                                  'Pms ID',
-                                                  'Appointment Postal Code',
-                                                  'Email',
-                                                  'Conference Date',
-                                                  'Appointment Date Time',
-                                                  'Deal Name',
-                                                  'Phone 4',
-                                                  'Phone 2',
-                                                  'Phone 3', ])
+merge_fields = ['Phone Number', 'Appointment State/Region', 'Appointment Date', 'Appointment Street Address', 'Consultant', 'Appointment City',
+                'Conference Venues', 'Pms ID', 'Appointment Postal Code', 'Email', 'Conference Date', 'Appointment Date Time', 'Deal Name', 'Phone 4', 'Phone 2', 'Phone 3']
+
+filtered_csv = pd.read_csv("output.csv", usecols=merge_fields)
 
 filtered_csv.to_csv("filtered_csv.csv", index=False)
 
@@ -37,23 +25,25 @@ template = 'template.docx'
 
 document = MailMerge(template)
 
-merge_fields = ['Phone Number', 'Appointment State/Region', 'Appointment Date', 'Appointment Street Address', 'Consultant', 'Appointment City',
-                'Conference Venues', 'Pms ID', 'Appointment Postal Code', 'Email', 'Conference Date', 'Appointment Date Time', 'Deal Name', 'Phone 4', 'Phone 2', 'Phone 3']
 
 with open('filtered_csv.csv') as file:
     reader = csv.reader(file, delimiter=',')
     next(reader)
     for merge_fields in reader:
         if merge_fields[11] != '':
-            appointment_datetime_object = datetime.strptime(merge_fields[11], '%Y-%m-%d')
-            appointment_datetime_format = datetime.strftime(appointment_datetime_object, '%d-%m-%Y')
+            appointment_datetime_object = datetime.strptime(
+                merge_fields[11], '%Y-%m-%d')
+            appointment_datetime_format = datetime.strftime(
+                appointment_datetime_object, '%d-%m-%Y')
         else:
-                appointment_datetime_format = merge_fields[11]
+            appointment_datetime_format = merge_fields[11]
         if merge_fields[5] != '':
-            conf_datetime_object = datetime.strptime(merge_fields[5], '%Y-%m-%d')
-            conf_datetime_format = datetime.strftime(conf_datetime_object, '%d-%m-%Y')
+            conf_datetime_object = datetime.strptime(
+                merge_fields[5], '%Y-%m-%d')
+            conf_datetime_format = datetime.strftime(
+                conf_datetime_object, '%d-%m-%Y')
         else:
-                conf_datetime_format = merge_fields[5]
+            conf_datetime_format = merge_fields[5]
         Deal_Name = merge_fields[12]
         document = MailMerge(template)
         document.merge(
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # HubMerge
-Combine Deals and Contacts exports from Hubspot into a single useable document that can be filtered and mail merged using a docx template
+Combine Deals and Contacts exports from Hubspot into a single useable document that can be filtered and mail merged using a docx template.
+
+#Instructions
+Export required information from Deals section on Hubspot, rename the file "deal.csv" and save it in the application directory. Select all properties for the export as we are going to filter out what we don't need anyway.
+Export required contact information relating to the Deals and save it as "contacts.csv" inside the application directory.
+Save your word document template with the matching merge fields as "template.docx" inside the application directory.
+
+Run the application.
+
+#Modifying the script for your own use
+
+You will likely need to adjust the fields being filtered to match what you need in your own template which you can do by editing the 'usecols' argument on the 'merge_fields' variable.
+
+merge_fields = ['Phone Number', 'Appointment State/Region', 'Appointment Date', 'Appointment Street Address', 'Consultant', 'Appointment City',
+                'Conference Venues', 'Pms ID', 'Appointment Postal Code', 'Email', 'Conference Date', 'Appointment Date Time', 'Deal Name', 'Phone 4', 'Phone 2', 'Phone 3']
+
+Adjust the 'document.merge' function to match the merge fields to their respective names inside of your template document:
+
+ document.merge(
+            Phone_2=merge_fields[0],
+            Phone_3=merge_fields[1],
+            Phone_4=merge_fields[2],
+            Pms_ID=merge_fields[3],
+            Phone_Number=merge_fields[4],
+            Conference_Date=conf_datetime_format,
+            Email=merge_fields[6],
+            Conference_Venues=merge_fields[7],
+            Appointment_Postal_Code=merge_fields[8],
+            Appointment_StateRegion=merge_fields[9],
+            Appointment_City=merge_fields[10],
+            Appointment_Date=appointment_datetime_format,
+            Deal_Name=merge_fields[12],
+            Appointment_Street_Address=merge_fields[13],
+            Consultant=merge_fields[14],
+            Appointment_Date_Time=merge_fields[15],
+        )
